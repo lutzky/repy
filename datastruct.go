c29625656d76da3940b7bb029b92fb1d5d42f17f
Ohad Lutzky <ohad@lutzky.net>
2017-08-02 10:26:49 +0100

    JSON output should be lowerCamelCase

--- datastruct.go
+++ datastruct.go
@@ -14,33 +14,35 @@ type Catalog []Faculty
 
 // Faculty represents a set of courses offered by a faculty.
 type Faculty struct {
-	Name    string
-	Courses []Course
+	Name    string   `json:"name"`
+	Courses []Course `json:"courses"`
 }
 
 // WeeklyHours represents the amount of weekly hours, by type, that a course
 // has.
 type WeeklyHours struct {
-	Lecture  uint
-	Tutorial uint
-	Lab      uint
-	Project  uint
+	Lecture  uint `json:"lecture"`
+	Tutorial uint `json:"tutorial"`
+	Lab      uint `json:"lab"`
+	Project  uint `json:"project"`
 }
 
 // Course represents information about a technion course.
 type Course struct {
-	ID               uint
-	Name             string
-	AcademicPoints   float32 // ...even though 2*points is always a uint :/
-	LecturerInCharge string
-	WeeklyHours      WeeklyHours
-	TestDates        []Date
-	Groups           []Group
+	ID               uint        `json:"id"`
+	Name             string      `json:"name"`
+	AcademicPoints   float32     `json:"academicPoints"`
+	LecturerInCharge string      `json:"lecturerInCharge"`
+	WeeklyHours      WeeklyHours `json:"weeklyHours"`
+	TestDates        []Date      `json:"testDates"`
+	Groups           []Group     `json:"groups"`
 }
 
 // Date is a timezone-free representation of a date
 type Date struct {
-	Year, Month, Day uint
+	Year  uint `json:"year"`
+	Month uint `json:"month"`
+	Day   uint `json:"day"`
 }
 
 // GroupType is the type of events in a group (applies to all events within a
@@ -90,17 +92,18 @@ func (gt *GroupType) UnmarshalJSON(b []byte) error {
 // Group represents a course's registration group (קבוצת רישום) and the events
 // it entails.
 type Group struct {
-	ID       uint
-	Teachers []string
-	Events   []Event
-	Type     GroupType
+	ID       uint      `json:"id"`
+	Teachers []string  `json:"teachers"`
+	Events   []Event   `json:"events"`
+	Type     GroupType `json:"type"`
 }
 
 // Event represents a singular weekly event within a course.
 type Event struct {
-	Day                time.Weekday
-	Location           string
-	StartHour, EndHour TimeOfDay
+	Day       time.Weekday `json:"day"`
+	Location  string       `json:"location"`
+	StartHour TimeOfDay    `json:"startHour"`
+	EndHour   TimeOfDay    `json:"endHour"`
 }
 
 // TimeOfDay is represented as "minutes since midnight".
