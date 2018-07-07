8ef12cf863ff3190241d83a7841a915f397a3194
Ohad Lutzky <ohad@lutzky.net>
2018-07-07 17:24:07 +0100

    Support faculties with dots in their name
    ...who knew.

--- repy.go
+++ repy.go
@@ -317,7 +317,7 @@ func (p *parser) parseCourseHeadInfo() error {
 }
 
 var (
-	facultyNameRegexp     = regexp.MustCompile(`\| *([א-ת ]+) *- *תועש תכרעמ *\|`)
+	facultyNameRegexp     = regexp.MustCompile(`\| *([א-ת\., ]+) *- *תועש תכרעמ *\|`)
 	facultySemesterRegexp = regexp.MustCompile(`\| *([א-ת" ]+) +רטסמס *\|`)
 )
 
